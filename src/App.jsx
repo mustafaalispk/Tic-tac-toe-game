1a5b67b7a5be1dbb5324b170540f012496315696
Mustafa Ali <mustafaalispk@yahoo.com>
2025-10-29 20:24:33 +0100

    Add state to select active player in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
+import {useState} from 'react';
 import { GameBoard } from "./components/GameBoard/GameBoard"
 import { Player } from "./components/Player/Player"
 
 function App() {
+  const [activePlayer, setActivePlayer] = useState('X');
+  const handleSelectSquare = () => {
+    setActivePlayer((currentActivePlayer) => currentActivePlayer === 'X' ? 'O' : 'X');
+  }
   return (
    <main>
      <div id="game-container">
-       <ol id="players">
-         <Player initialName="Player 1" symbol="X" />
-         <Player initialName="Player 2" symbol="0" />
+       <ol id="players" className="highlight-player">
+         <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'}/>
+         <Player initialName="Player 2" symbol="0" isActive={activePlayer === 'O'}/>
        </ol>
-       <GameBoard />
+       <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}/>
      </div>
    </main>
   )

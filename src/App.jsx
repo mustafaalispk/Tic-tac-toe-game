75dd5f0d1e1d34c56a4832529289578ef3c9dd47
Mustafa Ali <mustafaalispk@yahoo.com>
2025-10-30 16:42:27 +0100

    Add state for gameTurns and integrate logic in setGameTurns

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,37 @@
 import {useState} from 'react';
 import { GameBoard } from "./components/GameBoard/GameBoard"
 import { Player } from "./components/Player/Player"
+import { Log } from './components/Log/Log';
 
 function App() {
+  const [gameTurns, setGameTurns] = useState([]);
   const [activePlayer, setActivePlayer] = useState('X');
-  const handleSelectSquare = () => {
+  const handleSelectSquare = (rowIndex, colIndex) => {
     setActivePlayer((currentActivePlayer) => currentActivePlayer === 'X' ? 'O' : 'X');
+    setGameTurns((prevTurns) => {
+      let currentPlayer = 'X';
+      if (prevTurns.length > 0 && prevTurns[0].player === 'X') {
+        currentPlayer = 'O';
+      }
+      const updatedTurns = [
+        {square: {row: rowIndex, col: colIndex}, player: activePlayer},
+        ...prevTurns];
+      return updatedTurns;
+    });
   }
   return (
    <main>
      <div id="game-container">
-       <ol id="players" className="highlight-player">
-         <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'}/>
-         <Player initialName="Player 2" symbol="0" isActive={activePlayer === 'O'}/>
-       </ol>
-       <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}/>
+        <ol id="players" className="highlight-player">
+          <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'}/>
+          <Player initialName="Player 2" symbol="0" isActive={activePlayer === 'O'}/>
+        </ol>
+        <GameBoard 
+          onSelectSquare={handleSelectSquare} 
+          turns={gameTurns}
+        />
      </div>
+     <Log gameTurns={gameTurns}/>
    </main>
   )
 }

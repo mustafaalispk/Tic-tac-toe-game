d7fa734a39a1dc491195e803b627dd7ba2f9a14c
Mustafa Ali <mustafaalispk@yahoo.com>
2025-10-30 16:48:40 +0100

    Update game board dynamically according to player turns

--- src/components/GameBoard/GameBoard.jsx
+++ src/components/GameBoard/GameBoard.jsx
@@ -1,22 +1,17 @@
-import React,{useState} from 'react'
-
 const initialGameBoard = [
     [null, null, null],
     [null, null, null],
     [null, null, null]
 ]
 
-export const GameBoard = ({onSelectSquare, activePlayerSymbol}) => {
-  const[gameBoard, setGameBoard]=useState(initialGameBoard);
-
-  const handleSelectSquare = (rowIndex, colIndex) => {
-    setGameBoard((prevGameBoard) => {
-        const updatedBoard = [...prevGameBoard].map((innerArray) => [...innerArray]);
-        updatedBoard[rowIndex][colIndex]  = activePlayerSymbol;
-        return updatedBoard;
-    });       
-    onSelectSquare();
-  }
+export const GameBoard = ({onSelectSquare, turns}) => {
+    let gameBoard = initialGameBoard;
+    
+    for (const turn of turns) {
+        const {square, player} = turn;
+        const {row, col} = square;
+        gameBoard[row][col] = player;
+    }
   return (
     <ol id="game-board">
        {gameBoard.map((row, rowIndex) => {

8d17a9cf1730375d641a97039246195c28925c2c
Mustafa Ali <mustafaalispk@yahoo.com>
2025-10-31 07:29:51 +0100

    Fix symbol for Player component.

--- src/App.jsx
+++ src/App.jsx
@@ -14,26 +14,25 @@ const driveActivePlayer = (gameTurns) => {
 
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
-
+   
   const activePlayer = driveActivePlayer(gameTurns);
 
   const handleSelectSquare = (rowIndex, colIndex) => {
-    setActivePlayer((currentActivePlayer) => currentActivePlayer === 'X' ? 'O' : 'X');
     setGameTurns((prevTurns) => {
       const currentPlayer = driveActivePlayer(prevTurns);
 
       const updatedTurns = [
         {square: {row: rowIndex, col: colIndex}, player: currentPlayer},
         ...prevTurns];
-      return updatedTurns;
-    });
+        return updatedTurns;
+      });
   }
   return (
    <main>
      <div id="game-container">
         <ol id="players" className="highlight-player">
           <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'}/>
-          <Player initialName="Player 2" symbol="0" isActive={activePlayer === 'O'}/>
+          <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'}/>
         </ol>
         <GameBoard 
           onSelectSquare={handleSelectSquare} 
